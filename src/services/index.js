ca63981536f2c0dd434563ac1b5829c0ef4b4266
Davi Kawasaki <davishinjik@gmail.com>
2021-04-08 02:17:20 -0300

    feat: available services

---
+++ src/services/index.js
@@ -0,0 +1,9 @@
+const bankService = require('./bankService');
+const userService = require('./userService');
+const paymentService = require('./paymentService');
+
+module.exports = {
+  bankService,
+  userService,
+  paymentService,
+};
